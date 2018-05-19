fa1defaf1508207fc246a7444943b395b499741e
Denis Sirenko <sirenko.denis86@gmail.com>
2018-05-19 10:46:13 +0300

    first version

---
+++ src/js/main.js
@@ -0,0 +1,60 @@
+$('.offers-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+$('.reviews').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ web/js/main.js
@@ -0,0 +1,2 @@
+"use strict";$(".offers-slider").slick({slidesToShow:3,slidesToScroll:1,arrows:!1,autoplay:!0,autoplaySpeed:2e3}),$(".reviews").slick({slidesToShow:4,slidesToScroll:1,arrows:!1,autoplay:!0,autoplaySpeed:2e3});
+//# sourceMappingURL=main.js.map
