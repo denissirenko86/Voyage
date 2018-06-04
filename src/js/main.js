5305a6993739724b002a35d82e58ae3cad87c4bb
Denis Sirenko <sirenko.denis86@gmail.com>
2018-06-04 17:09:34 +0300

    final

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,77 @@
-$('.offers-slider').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  arrows: false,
-  autoplay: true,
-  autoplaySpeed: 2000,
-});
+// $('.offers-slider').slick({
+//   slidesToShow: 4,
+//   slidesToScroll: 1,
+//   arrows: false,
+//   autoplay: true,
+//   autoplaySpeed: 2000,
+// });
 
-$('.reviews').slick({
+$('.offers-slider').slick({
+  dots: false,
+  infinite: true,
   slidesToShow: 4,
   slidesToScroll: 1,
   arrows: false,
   autoplay: true,
-  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+$('.reviews').slick({
+  dots: false,
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: false,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
 
 // Scroll 
